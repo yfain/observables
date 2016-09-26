71cc843e435d2bd8c0f8b4efd1c12ee91509461d
Yakov Fain <yakovfain@gmail.com>
2016-09-26 15:03:31 -0400

    Adding catch to async pipe

--- code/http_websocket_samples/client/app/main-asyncpipe.ts
+++ code/http_websocket_samples/client/app/main-asyncpipe.ts
@@ -3,6 +3,9 @@ import { NgModule, Component }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpModule, Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/from';
 import { Observable} from "rxjs/Observable";
 
 @Component({
@@ -13,15 +16,26 @@ import { Observable} from "rxjs/Observable";
        {{product.title}}
     </li>
   </ul>
+  
   `})
 class AppComponent {
 
   products: Observable<Array<string>>;
+  errorMessage: Observable;
 
   constructor(private http: Http) {
 
    this.products = this.http.get('/products')
-        .map(res => res.json()); 
+        .map(res => res.json())
+        .catch( err => {
+                console.log("Can't get product details. Error code: %s, URL: %s ",  err.status, err.url)
+                let errors: Array = [];
+                errors.push("Can't get product details.");
+                //this.errorMessage = Observable.from(errors);
+                this.errorMessage =Observable.empty();
+                return this.errorMessage;
+                });
+
   }
 }
 
